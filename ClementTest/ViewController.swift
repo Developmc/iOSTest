12e2fbc9edec08914fca8ab0037ae6232fca885f
heipacker <heipackermail@gmail.com>
2019-06-03 11:25:16 +0800

    Initial Commit

---
+++ ClementTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClementTest
+//
+//  Created by aimee on 2019/6/3.
+//  Copyright © 2019 aimee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClementTestTests/ClementTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ClementTestTests.swift
+//  ClementTestTests
+//
+//  Created by aimee on 2019/6/3.
+//  Copyright © 2019 aimee. All rights reserved.
+//
+
+import XCTest
+@testable import ClementTest
+
+class ClementTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
